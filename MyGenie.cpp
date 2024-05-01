04b44f1d715bb320ea96d08946c20a1d768bec95
Mahmudul Hasan <cs.mahmud@gmail.com>
2024-05-01 14:39:05 -0600

    Changed Name

---
+++ MyGenie.cpp
@@ -0,0 +1,13 @@
+#include "MyContainer.h"
+using namespace std;
+
+
+MyContainer::MyContainer() {
+    
+}
+
+
+int MyContainer::load(char *filename, int color_mode) {
+    return 0; 
+}
+
---
+++ MyGenie.h
@@ -0,0 +1,18 @@
+#ifndef MY_GENIE_H
+#define MY_GENIE_H
+
+#include<vector>
+
+using namespace std;
+
+class MyGenie {
+ public:
+    MyGenie();
+    ~MyGenie();
+
+    int load(char* filename);
+
+ private:
+};
+
+#endif
